27f2b50226781728107cbef7a1c9bb7c41b5bb4b
Anatoly Kalin <ttldtor@gmail.com>
2018-08-28 14:32:04 +0300

    additional params

--- bin/hpsm.py
+++ bin/hpsm.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 import base64
 import json
 import sys
@@ -12,9 +13,33 @@ def send_message(settings):
     title_value = settings.get("title")
     description_value = settings.get("description")
     category_value = settings.get("category", "incidents")
+    subcategory_value = settings.get("subcategory", "Другое")
+    product_type_value = settings.get("productType", "Другое")
     from_value = settings.get("from", "splunk")
-    body = r'{"tlmrSplunkMon": {"title": "%s", "description": ["%s"], "category": "%s", "from": "%s"}}' % (
-        title_value, description_value, category_value, from_value)
+    affected_item_value = settings.get("affectedItem", "CI1127974")
+    location_value = settings.get("location", "5a12997da100a05f506e1e38")
+    callback_contact_value = settings.get("callbackContact", "splunk")
+    contact_name_value = settings.get("contactName", "splunk")
+    assignment_value = settings.get("assignment", "SPLUNK")
+
+    body = json.dumps(
+        {
+            "tlmrSplunkMon": {
+                "title": title_value,
+                "description": [description_value],
+                "category": category_value,
+                "subcategory": subcategory_value,
+                "product.type": product_type_value,
+                "from": from_value,
+                "affected.item": affected_item_value,
+                "location": location_value,
+                "callback.contact": callback_contact_value,
+                "contact.name": contact_name_value,
+                "assignment": assignment_value
+            }
+        }
+    )
+
     print >> sys.stderr, 'DEBUG Calling url="%s" with body=%s' % (base_url_value, body)
     request = urllib2.Request(base_url_value, body, {"Content-Type": "application/json"})
     request.add_header("Authorization",
